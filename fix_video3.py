206fc0cb94571d85c0ee8ee79b95a86d56c47757
Brian Wu <m11362010@newmedia.tnua.edu.tw>
2026-06-25 15:59:00 +0800

    Simpler fix: remove description prefix and fix nbsp

--- fix_video3.py
+++ fix_video3.py
@@ -5,23 +5,27 @@ with open('index.html', 'r', encoding='utf-8') as f:
     html = f.read()
 orig = len(html)
 
-old = (
-    '<div class="wm" contenteditable="true">Video &middot; 2026 &middot; 點擊前往 YouTube'
-    '&nbsp;<div>創作團隊：武威桀 陳彥甫 林慧儒 楊茗柯 鄭欣怡 謝浩理 林廣騏 江育葳</div>'
-    '<div>進行式蒸發：動作捕捉舞者身體，生成數位分身漂浮展出空間；觀眾透過手機界面出價競標，拍下的分身隨買家下載離開，流標者於演出結束後被消失。將「上傳限時動態」的比喻，施加於活生生的數位身體之上。</div></div>'
-)
-
-new = (
-    '<div class="wm" contenteditable="true">Video &middot; 2026 &middot; 點擊前往 YouTube'
-    '<br><div>創作團隊：武威桀 陳彥甫 林慧儒 楊茗柯 鄭欣怡 謝浩理 林廣騏 江育葳</div>'
-    '<div>動作捕捉舞者身體，生成數位分身漂浮展出空間；觀眾透過手機界面出價競標，拍下的分身隨買家下載離開，流標者於演出結束後被消失。將「上傳限時動態」的比喻，施加於活生生的數位身體之上。</div></div>'
-)
+# Just replace the description div content - use short unique fragments
+old = '進行式蒸發：動作捕捉舞者身體，生成數位分身漂浮展出空間；觀眾透過手機界面出價競標，拍下的分身隨買家下載離開，流標者於演出結束後被消失。將「上傳限時動態」的比喻，施加於活生生的數位身體之上。'
+new = '動作捕捉舞者身體，生成數位分身漂浮展出空間；觀眾透過手機界面出價競標，拍下的分身隨買家下載離開，流標者於演出結束後被消失。將「上傳限時動態」的比喻，施加於活生生的數位身體之上。'
 
 if old in html:
     html = html.replace(old, new, 1)
-    print('Fixed.')
+    print('Removed prefix OK.')
 else:
-    print('Pattern not found.')
+    print('Not found. Searching for video item...')
+    idx = html.find('WQ0qF_Lh0-k')
+    if idx != -1:
+        print(repr(html[idx:idx+500]))
+    else:
+        print('Video item missing entirely.')
+
+# Also fix &nbsp; before team div -> <br>
+html = html.replace(
+    'YouTube&nbsp;<div>創作團隊：武威桀',
+    'YouTube<br><div>創作團隊：武威桀',
+    1
+)
 
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(html)
