ee66dab066588017fbb342d069b405fcd4bfcc3b
Brian Wu <m11362010@newmedia.tnua.edu.tw>
2026-06-25 14:04:49 +0800

    Re-trigger: add 限時動態 Action! Figure to video tab

--- add_video_work.py
+++ add_video_work.py
@@ -20,10 +20,12 @@ else:
         '          </div>\n'
         '        </div>\n'
         '      </a>\n'
-        '      <div class="wio"><div class="wt" contenteditable="true">限時動態 Action! Figure</div>'
-        '<div class="wm" contenteditable="true">Video · 2026 · 點擊前往 YouTube'
-        '&nbsp;<div>創作團隊：武威桀 陳彥甫 林慧儒 楊茗柯 鄭欣宜 謝浩理 林廣騎 江育噌</div>'
-        '<div>進行式蒸發：動作捕捉舞者身體，生成數位分身漂浮展出空間；觀眾透過手機界面出價競標，拍下的分身隨買家磨磟離開，流標者於演出結束後消失。將「上傳限時動態」的比喻，施加於活生生的身體之上。</div></div></div>\n'
+        '      <div class="wio">'
+        '<div class="wt" contenteditable="true">限時動態 Action! Figure</div>'
+        '<div class="wm" contenteditable="true">Video &middot; 2026 &middot; 點擊前往 YouTube'
+        '&nbsp;<div>創作團隊：武威桁 陳彥甫 林慧儲 楊茗柯 鄭欣宜 謝浩理 林廣騎 江育瘍</div>'
+        '<div>進行式蒸發：動作捕捉舞者身體，生成數位分身漂浮展出空間；觀眾透過手機界面出價競標，拍下的分身隨買家磁碟離開，流標者於演出結束後消失。將「上傳限時動態」的比喻，施加於活生生的身體之上。</div>'
+        '</div></div>\n'
         '      <button class="db" onclick="di(this.parentElement)">✕</button>\n'
         '    </div>\n'
     )
@@ -32,7 +34,7 @@ else:
         html = html.replace(grid_end, new_item + grid_end, 1)
         print('Inserted before /grid marker')
     else:
-        print('ERROR: grid end marker not found')
+        print('ERROR: grid end marker not found, len=', len(html))
 
 with open('index.html', 'w', encoding='utf-8') as f:
     f.write(html)
